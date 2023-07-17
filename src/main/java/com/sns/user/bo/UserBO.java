65f2917729754e4e9170cf3677e94ea61961bb47
DESKTOP-LV8U6T0\KANG SUNGTAE <sickc@naver.com>
2023-07-18 01:35:38 +0900

    같은 ㅣㅇ유

--- src/main/java/com/sns/user/bo/UserBO.java
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -1,10 +1,12 @@
 package com.sns.user.bo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sns.user.dao.UserRepository;
 import com.sns.user.entity.UserEntity;
 
+@Service
 public class UserBO {
 
 	@Autowired
